3aa8db751387240c0de45c510109734e5a1cb249
WTF-ATuan <62327517+WTF-ATuan@users.noreply.github.com>
2023-11-02 12:50:04 +0800

    added scene art and map

---
+++ Assets/GravityBall/GravityBallActionHandler.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace{
+	public class GravityBallActionHandler : MonoBehaviour{
+		private ParticleSystem _childParticle;
+		[SerializeField] private int boundsMax = 0;
+
+		private int _boundsCount;
+		private void Start(){
+			_childParticle = GetComponentInChildren<ParticleSystem>();
+		}
+
+		private void OnCollisionEnter(Collision collision){
+			_boundsCount += 1;
+			if(_boundsCount > boundsMax){
+				Disperse();
+			}
+		}
+
+		private void Disperse(){
+			_childParticle.transform.SetParent(null);
+			_childParticle.Play();
+			gameObject.SetActive(false);
+		}
+	}
+}
