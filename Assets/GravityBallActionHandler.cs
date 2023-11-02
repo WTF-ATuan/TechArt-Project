4bad4a91c3878747b0693ec3336bbe71fdb9b22c
WTF-ATuan <62327517+WTF-ATuan@users.noreply.github.com>
2023-11-02 12:11:16 +0800

    added gravity action behavior, when ball hit ground will disperse

--- Assets/GravityBallActionHandler.cs
+++ Assets/GravityBallActionHandler.cs
@@ -2,6 +2,16 @@
 
 namespace DefaultNamespace{
 	public class GravityBallActionHandler : MonoBehaviour{
-		
+		private ParticleSystem _childParticle;
+
+		private void Start(){
+			_childParticle = GetComponentInChildren<ParticleSystem>();
+		}
+
+		private void OnCollisionEnter(Collision collision){
+			_childParticle.transform.SetParent(null);
+			_childParticle.Play();
+			gameObject.SetActive(false);
+		}
 	}
 }
